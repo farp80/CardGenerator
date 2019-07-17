4cf057a727fdf0b46b106007ab754b532bdf2449
farp80 <farp80@gmail.com>
2019-07-17 01:08:48 +0000

    initial version

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,25 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let suit = suits()[selectSuit()];
+  let card = cardsNumbers()[selectCard()];
+
+  let center = document.querySelector("#center");
+  center.innerHTML = card;
+};
+
+let selectSuit = () => {
+  return Math.floor(Math.random() * 4);
+};
+
+let selectCard = () => {
+  return Math.floor(Math.random() * 13);
+};
+
+let suits = () => {
+  return ["&hearts;", "&clubs;", "&diams;", "&spades;"];
+};
+
+let cardsNumbers = () => {
+  return [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 };
